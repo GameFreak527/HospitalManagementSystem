d9142c979c6c5848646f8728165ae49fe0ccc152
GameFreak527 <shivrana527@gmail.com>
2019-03-13 17:03:18 -0400

    Registration of Patient Added
    functionality of Registration of Patient added into the system.

--- Hospital Management System/ConnectionClass.cs
+++ Hospital Management System/ConnectionClass.cs
@@ -18,7 +18,9 @@ namespace Hospital_Management_System
             //This is my connection string
             //Comment mine out and add yours to use this classes methods
 
-            cn = new SqlConnection(@"Data Source=LAPTOP-47BNNCUR\COMP229_SQL;Initial Catalog=Hospital_ManagementDB;Integrated Security=True");
+            //Gab's
+            //cn = new SqlConnection(@"Data Source=LAPTOP-47BNNCUR\COMP229_SQL;Initial Catalog=Hospital_ManagementDB;Integrated Security=True");
+            cn = new SqlConnection(@"Data Source=GameFreak;Initial Catalog=Hospital_ManagementDB;Integrated Security=True");
         }
 
 
@@ -54,6 +56,42 @@ namespace Hospital_Management_System
             {
                 cn.Close();
             }
+        }
+
+        public static int addPatient(Patient patient)
+        {
+            int result =0;
+            SqlCommand cmd = new SqlCommand(@"insert into [Patient] (Ward, FirstName, LastName, PhoneNumber, Address) values(@wardId, @firstName, @lastName, @phoneNumber, @address)", cn);
+
+            cmd.Parameters.Add("@wardId", System.Data.SqlDbType.Int);
+            cmd.Parameters.Add("@firstName", System.Data.SqlDbType.VarChar);
+            cmd.Parameters.Add("@lastName", System.Data.SqlDbType.VarChar);
+            cmd.Parameters.Add("@phoneNumber", System.Data.SqlDbType.VarChar);
+            cmd.Parameters.Add("@address", System.Data.SqlDbType.VarChar);
+
+            cmd.Parameters["@wardId"].Value = patient.Ward;
+            cmd.Parameters["@firstName"].Value = patient.FirstName;
+            cmd.Parameters["@lastName"].Value = patient.LastName;
+            cmd.Parameters["@phoneNumber"].Value = patient.PhoneNumber;
+            cmd.Parameters["@address"].Value = patient.Address;
+
+            try
+            {
+                cn.Open();
+                result = cmd.ExecuteNonQuery();
+
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+                new Exception("ERROR : " + e.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return result;
         }
     }
 }
---
+++ Hospital Management System/RegisterPatient.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hospital_Management_System
+{
+    public partial class RegisterPatient : System.Web.UI.Page
+    {
+        HospitalDBEntities context = new HospitalDBEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Hospital hospital = context.Hospitals.FirstOrDefault(x => x.HospitalID == 1);
+            List<int> wards = (from a in hospital.Wards
+                               select a.WardID).ToList();
+            wardList.DataSource = wards;
+            wardList.DataBind();
+        }
+
+        protected void submitBtn_Click(object sender, EventArgs e)
+        {
+            String firstName = firstNameTxtBox.Text;
+            String lastName = lastNameTxtBox.Text;
+            String address = addressTxtBox.Text;
+            String phoneNumber = phoneTxtBox.Text;
+            int wardId = int.Parse(wardList.SelectedValue);
+
+           //Tried using Entity Framework but didnt worked
+            //Ward ward = context.Wards.FirstOrDefault(x => x.WardID == wardId);
+            //ward.Patients.Add(new Patient
+            //{
+            //    FirstName = firstName,
+            //    LastName = lastName,
+            //    Address = address,
+            //    PhoneNumber = phoneNumber
+            //});
+
+            Patient newPatient = new Patient
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Address = address,
+                PhoneNumber = phoneNumber,
+                Ward = wardId
+            };
+
+            try
+            {
+                //Means the insertion is done
+                if (ConnectionClass.addPatient(newPatient) == 1)
+                {
+                    errorMessageDiv.Visible = true;
+                    errorMessageDiv.Attributes["class"] = "alert alert-success";
+                    errorMessage.Text = "Patient Is Registered";
+                }
+                    
+            }
+            catch (NullReferenceException ex)
+            {
+                errorMessageDiv.Visible = true;
+                errorMessage.Text = ex.Message;
+            }
+            
+
+        }
+    }
+}
